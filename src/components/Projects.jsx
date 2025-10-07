4865e04ce8d8027d830c5322cba5c37175922205
Davi Negreiros <davi.negreiiros36@gmail.com>
2025-10-06 23:52:16 -0300

    fix: organize

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 const PROJECTS = [
-  {
-    title: 'MyBookStore (Flutter)',
-    desc: 'App de gerenciamento de livros e funcionários criado como teste prático. Arquitetura com BLoC, suporte a flavors (QA/Produção), integração com Firebase Analytics, imagens em base64 e papéis ADMIN/EMPLOYEE.',
-    tech: 'Flutter, BLoC, Firebase'
-  },
+  
   {
     title: 'Agendamento Pet Mobile',
     desc: 'Aplicativo mobile para agendamento de serviços pet, permitindo cadastro de pets, agendamento de horários, notificações e integração com backend. Projeto disponível em: https://github.com/davinegreiiros/agendamento-app-pet',
     tech: 'Flutter, Firebase, Bloc, API-REST'
+  },
+  {
+    title: 'MyBookStore (Flutter)',
+    desc: 'App de gerenciamento de livros e funcionários criado como teste prático. Arquitetura com BLoC, suporte a flavors (QA/Produção), integração com Firebase Analytics, imagens em base64 e papéis ADMIN/EMPLOYEE.',
+    tech: 'Flutter, BLoc, Firebase'
   },
   {
     title: 'Chess System Java',
