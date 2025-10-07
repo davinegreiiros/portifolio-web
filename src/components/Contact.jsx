895d65dc998cea52df9b8814713af998f0917d41
Davi Negreiros <davi.negreiiros36@gmail.com>
2025-10-07 00:21:37 -0300

    fix: ajust link github

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact(){
       <ul>
         <li>Email: <a href="mailto:davi.negreiiros36@gmail.com">davi.negreiiros36@gmail.com</a></li>
         <li>LinkedIn: <a href="https://www.linkedin.com/in/davi-negreiros-31b942286/" target="_blank" rel="noreferrer">linkedin.com/in/davi-negreiros-31b942286</a></li>
-        <li>GitHub: <a href="https://github.com/davinegreiros" target="_blank" rel="noreferrer">github.com/davinegreiros</a></li>
+        <li>GitHub: <a href="https://github.com/davinegreiiros" target="_blank" rel="noreferrer">github.com/davinegreiros</a></li>
       </ul>
     </section>
   )
